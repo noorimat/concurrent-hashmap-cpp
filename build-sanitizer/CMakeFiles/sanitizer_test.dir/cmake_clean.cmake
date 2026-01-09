4730e72c835bb9906b2eefeb5961e61b8790c609
Noor Imat <binnur.imat@gmail.com>
2026-01-09 02:45:27 -0500

    Added performance visualization graphs and plotting script

---
+++ build-sanitizer/CMakeFiles/sanitizer_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sanitizer_test.dir/src/sanitizer_test.cpp.o"
+  "CMakeFiles/sanitizer_test.dir/src/sanitizer_test.cpp.o.d"
+  "sanitizer_test"
+  "sanitizer_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sanitizer_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build-sanitizer/CMakeFiles/stress_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stress_test.dir/src/stress_test.cpp.o"
+  "CMakeFiles/stress_test.dir/src/stress_test.cpp.o.d"
+  "stress_test"
+  "stress_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stress_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
